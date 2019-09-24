da4df83a8146089e9ea0d5dc66b0e63f6a38a2f6
Quentin BARTOLETTI <qb787926@MI115-01.iem>
2019-09-24 16:34:12 +0200

    premier commit

---
+++ Orange.java
@@ -0,0 +1,25 @@
+public class Orange{
+
+	private double prix;
+	private String origine;
+	
+	public static void main (String[]args){
+		Orange o1= new Orange(1.20,"Argentine");
+		System.out.println(o1.getPrix());
+		System.out.println(o1.getOrigine());
+	}
+	
+	public Orange(double p, String o){
+	
+	if(p<0)
+		this.prix=0.12;
+	else
+		this.prix=p;
+		
+	this.origine=o;
+	}
+	
+	public double getPrix(){return this.prix;}
+	public String getOrigine(){return this.origine;}
+	
+}
